5bfb4b9f797e4fd06997fe7a571bcd5d5335c1a1
IbanL <iban.letoile@gmail.com>
2024-11-06 11:16:09 +0100

    exercice 1

---
+++ 01_trings/script.js
@@ -0,0 +1,24 @@
+console.log("JavaScript est super.");
+
+console.log("JavaScript c'est facile.");
+
+console.log(`"L'échec est le fondement de la réussite".`);
+
+console.log(" Afficher un \\ n'est pas si évident.");
+
+console.log("Ceci est la première ligne\nCeci est la deuxième ligne");
+
+console.log(`Un jour le JavaScript n'aura plus de secrets pour moi
+Lire du code ne sera plus une gageure mais une joie
+Mes doigts seront habitués à tous ces symboles particuliers
+Et ma créativité de développeur pourra enfin s'exprimer `);
+
+let nom = "bob";
+
+console.log(`Bonjour `+nom+`, aujourd'hui nous faisons une promo "la fidélité paye" !
+
+Tom doit écrire une chaîne de caractère qui permet d'écrire cette phrase pour chaque client. Pour l'instant il essaye de faire fonctionner son code avec le prénom "Bob". `);
+
+console.log(`\t\tBonjour `+nom+`,
+
+Aujourd'hui nous faisons une promo "la fidélité paye" !`)
