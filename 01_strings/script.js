d0f2f711aca7a5664d1b8d0222d9c8354fef6827
IbanL <iban.letoile@gmail.com>
2024-11-06 11:20:33 +0100

    exercice1 avec comm

--- 01_strings/script.js
+++ 01_strings/script.js
@@ -1,24 +1,33 @@
+//exercice 0
 console.log("JavaScript est super.");
 
+//exercice 1
 console.log("JavaScript c'est facile.");
 
+//exercice 2
 console.log(`"L'échec est le fondement de la réussite".`);
 
+//exercice 3
 console.log(" Afficher un \\ n'est pas si évident.");
 
+//exercice 4
 console.log("Ceci est la première ligne\nCeci est la deuxième ligne");
 
+//exercice 5
 console.log(`Un jour le JavaScript n'aura plus de secrets pour moi
 Lire du code ne sera plus une gageure mais une joie
 Mes doigts seront habitués à tous ces symboles particuliers
 Et ma créativité de développeur pourra enfin s'exprimer `);
 
+
+//Chassez le bogue ! 0
 let nom = "bob";
 
 console.log(`Bonjour `+nom+`, aujourd'hui nous faisons une promo "la fidélité paye" !
 
 Tom doit écrire une chaîne de caractère qui permet d'écrire cette phrase pour chaque client. Pour l'instant il essaye de faire fonctionner son code avec le prénom "Bob". `);
 
+//Chassez le bogue ! 1
 console.log(`\t\tBonjour `+nom+`,
 
 Aujourd'hui nous faisons une promo "la fidélité paye" !`);
