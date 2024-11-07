6459836c4848f539d033295074f2901f99530f99
IbanL <iban.letoile@gmail.com>
2024-11-07 14:13:36 +0100

    exercice3

---
+++ 03_bolean/script.js
@@ -0,0 +1,18 @@
+//exercice0
+// Remplacez uniquement les "XX" par des opérateurs
+// de comparaison et/ou des opérateurs logiques
+console.log(65 < 70 && 4 < 3);
+
+//exercice2
+//  Remplacez uniquement les "XX" par des opérateurs
+// de comparaison et/ou des opérateurs logiques
+console.log((170 >= 150 || 15 > 16)&& 79 <80);
+
+//exercice3
+// Remplacez uniquement les "XX" par des opérateurs
+// de comparaison et/ou des opérateurs logiques
+console.log((10 < 12 || 110 < 120) || (58 > 40 && 58 < 60));
+
+//Chassez le bogue ! 0
+//  En utilisant uniquement des parenthèses, corrigez le bogue !
+console.log((150 >= 120 || 16 > 15) && 95 < 90);
